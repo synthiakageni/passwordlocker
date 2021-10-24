0b008d52b149e61b98e8c0329103320e802f7b23
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-10-24 12:17:40 +0300

    add _init_ method

--- user.py
+++ user.py
@@ -5,3 +5,11 @@ class User:
     """
     Class that generates new instances of contacts.
     """
+users_list = []  # Empty contact list
+
+def __init__(self, username, password):
+
+      # docstring removed for simplicity
+
+        self.username = username
+        self.password = password
