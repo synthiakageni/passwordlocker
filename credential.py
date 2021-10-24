567a3a8d996ebd9618566cbc70b0711e9f5fbeaf
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-10-24 18:02:06 +0300

    add class method _init_

--- credential.py
+++ credential.py
@@ -16,3 +16,10 @@ class Credential:
             if (user.username == username and user.password == password):
                 firstuser == user.username
         return firstuser
+    def __init__(self, account, userName, passWord):
+        '''
+        method that initializes the credentials to be stored
+        '''
+        self.account = account
+        self.userName = userName
+        self.passWord = passWord
