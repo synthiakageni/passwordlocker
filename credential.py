d862c4341b2f7661f5b3251fe90b71a5418fcb8b
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-10-24 18:00:27 +0300

    add class method authenticate_user

--- credential.py
+++ credential.py
@@ -5,3 +5,14 @@ class Credential:
     the blueprint for creating new objects for credentials
     '''
     credentials_list = []
+    @classmethod
+    def authenticate_user(cls, username, password):
+        '''
+        classmethod
+        check whether the user is in our list
+        '''
+        firstuser = ""
+        for user in User.users_list:
+            if (user.username == username and user.password == password):
+                firstuser == user.username
+        return firstuser
