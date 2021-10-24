1ae27c17c43b42db07ea9865a626fe560ab5e874
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-10-24 18:19:40 +0300

    add method setUp()

--- test_credential.py
+++ test_credential.py
@@ -5,3 +5,9 @@ class TestCredential(unittest.TestCase):
     '''
     Test class that defines the test cases for the behaviour of the credential class
     '''
+    def setUp(self):
+        '''
+        method to run before each test case 
+        '''
+        self.new_credential = Credential("Twitter", "kageni", "kagenikageni")
+        
