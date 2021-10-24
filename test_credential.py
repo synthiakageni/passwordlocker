11e5afe16b1fa3a3c9262c124986718be1e40486
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-10-24 18:17:32 +0300

    create class TestCredential

--- test_credential.py
+++ test_credential.py
@@ -1,3 +1,7 @@
 import unittest 
 from credential import Credential
 import pyperclip
+class TestCredential(unittest.TestCase):
+    '''
+    Test class that defines the test cases for the behaviour of the credential class
+    '''
