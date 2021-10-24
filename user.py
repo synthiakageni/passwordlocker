889f80272a00280e2b43adc377fd62518e033c26
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-10-24 12:53:04 +0300

    create class method authenticate_user

--- user.py
+++ user.py
@@ -17,9 +17,20 @@ def save_user(self):
         '''
         method that saves a new instance of a user into the list
         '''
-        User.users_list.append(self)
+        User.users_list.append()
 def delete_user(self):
         '''
         method for deleting a saved user from the users list
         '''
-        User.users_list.remove(self)    
+        User.users_list.remove()
+@classmethod
+def authenticate_user(cls, username, password):
+        '''
+        classmethod
+        check whether the user is in our list
+        '''
+        firstuser = ""
+        for user in User.users_list:
+            if (user.username == username and user.password == password):
+                firstuser == user.username
+        return firstuser      
